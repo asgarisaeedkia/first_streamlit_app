0625c789052f8d5ba91b6bb407f670a452e96c35
asgarisaeedkia <153580974+asgarisaeedkia@users.noreply.github.com>
2023-12-20 15:52:23 +1030

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
-if streamlist.button('Get the fruit list'):
+if streamlit.button('Get the fruit list'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 streamlit.stop()
